bc4126f924c16b3942ea7639afdbd43f7f48c962
zer0kool <dwebcrew@protonmail.com>
2017-04-07 16:16:55 -0700

    test

--- Computing/settings/production.py
+++ Computing/settings/production.py
@@ -14,4 +14,4 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOST = ['arcane-dawn-84704.herokuapp.com']
