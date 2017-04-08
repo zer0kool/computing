b1cc1ee274d779cc34ad3fc1d548b10cc2792922
zer0kool <dwebcrew@protonmail.com>
2017-04-07 18:48:47 -0700

    sd

--- Computing/settings/production.py
+++ Computing/settings/production.py
@@ -8,10 +8,15 @@ try:
     from .local import *
 except ImportError:
     pass
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOST = ['*']
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
